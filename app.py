3b0a063cceb64a583eadb19eb9eef08a8e96022f
Laryn Qi <larynqi@berkeley.edu>
2021-10-25 01:02:08 -0700

    full v1

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# token = ''
+token = ''
 
 # # from slackclient import SlackClient
 
-# from slack_sdk import WebClient
-# from slack_sdk.errors import SlackApiError
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import re
 
 # client = WebClient(token)
 
@@ -18,38 +19,170 @@
 
 import os
 from slack_sdk.signature import SignatureVerifier
+
+from slack_bolt import App, Say
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+
 signature_verifier = SignatureVerifier(
-    signing_secret='secret'
+    signing_secret=''
     # signing_secret=os.environ["SLACK_SIGNING_SECRET"]
 )
 
 from slack_sdk.webhook import WebhookClient
 
 from flask import Flask, request, make_response
+from utils import init_db, read_db, write_db, email_db
 app = Flask(__name__)
 
+init_db()
+caught, score, spot, images = read_db()
+
+# @app.route("/spot", methods=["POST"])
+# def spot():
+#     # Verify incoming requests from Slack
+#     # https://api.slack.com/authentication/verifying-requests-from-slack
+
+#     # print(request.form)
+#     request.get_data()
+#     if request.form.get('token', '') != 'XMCKbjVB4FN7ajPyDPXSSzMb':
+#         print('failed 1.')
+#         return make_response("invalid request", 403)
+#     # print(request.headers)
+#     # request.get_data()
+#     if not signature_verifier.is_valid_request(request.data, request.headers):
+#         print('failed 2.')
+#         return make_response("invalid request", 403)
+#     # if not signature_verifier.is_valid(
+#     #     body=request.get_data(),
+#     #     timestamp=request.headers.get("X-Slack-Request-Timestamp"),
+#     #     signature=request.headers.get("X-Slack-Signature")):
+#     #     return make_response("invalid request", 403)
+
+#     print("HI", request.form)
+#     timestamp = request.headers.get("X-Slack-Request-Timestamp")
+#     # Handle a slash command invocation
+#     if "command" in request.form \
+#         and request.form["command"] == "/spot":
+#         response_url = request.form["response_url"]
+#         text = request.form["text"]
+#         webhook = WebhookClient(response_url)
+#         # Send a reply in the channel
+#         response = webhook.send(text=f"You said '{text}'")
+    
+#         client = WebClient(token=token)
+#         try:
+#             response = client.chat_postMessage(channel='#bot-dev', text="Hello world!")
+#             response = client.reactions_add(channel="#bot-dev", name="thumbsup", timestamp=timestamp)
+#             assert response["message"]["text"] == "Hello world!"
+#         except SlackApiError as e:
+#             assert e.response["ok"] is False
+#             assert e.response["error"]
+#             print(f"Got an error: {e.response['error']}")
+
+
+#         # Acknowledge this request
+#         return make_response("", 200)
+
+#     return make_response("", 404)
+# bolt_app = App(token=token)
+
+bolt_app = App(token=token, signing_secret=os.environ.get("SIGNING_SECRET"))
+handler = SlackRequestHandler(bolt_app)
 @app.route("/slack/events", methods=["POST"])
-def slack_app():
-    # Verify incoming requests from Slack
-    # https://api.slack.com/authentication/verifying-requests-from-slack
-    if not signature_verifier.is_valid(
-        body=request.get_data(),
-        timestamp=request.headers.get("X-Slack-Request-Timestamp"),
-        signature=request.headers.get("X-Slack-Signature")):
-        return make_response("invalid request", 403)
+def handle_events():
+    return handler.handle(request)
 
-    # Handle a slash command invocation
-    if "command" in request.form \
-        and request.form["command"] == "/reply-this":
-        response_url = request.form["response_url"]
-        text = request.form["text"]
-        webhook = WebhookClient(response_url)
-        # Send a reply in the channel
-        response = webhook.send(text=f"You said '{text}'")
-        # Acknowledge this request
-        return make_response("", 200)
+# @bolt_app.message("spot")
+# def respond(event, say):
+#     user = event.get("user")
+#     print(event)
+#     say(f"spotted <@{user}>")
+#     client = WebClient(token=token)
+#     response = client.reactions_add(channel=event['channel'], name="white_check_mark", timestamp=event['ts'])
+#     # response = client.reactions_add(channel="#bot-dev", name="thumbsup", timestamp=payload['ts'])
 
-    return make_response("", 404)
+client = WebClient(token=token)
+SPOT_WORDS = ["spot", "spotted", "codespot", "codespotted"]
+USER_PATTERN = r"<@[a-zA-Z0-9]{11}>"
+
+prev = [None, None]
+
+@bolt_app.event({
+    "type": "message",
+    "subtype": "file_share"
+})
+def log_spot(event, say):
+    print(event)
+
+    if any([w in event.get('text', '').lower() for w in SPOT_WORDS]):
+        spotter = event['user']
+        found_spotted = re.search(USER_PATTERN, event['text'])
+        if not found_spotted:
+            return
+        spotted = found_spotted[0]
+        spot[spotter] = spot.get(spotter, 0) + 1
+        caught[spotted] = caught.get(spotted, 0) + 1
+        for image in event['files']:
+            images[spotted] = images.get(spotted, []) + [image['url_private']]
+        global prev
+        if spotter == prev[0]:
+            prev[1] += 1
+            if prev[1] >= 3:
+                say(f"<@{spotter}> is on fire 🥵")
+        else:
+            prev[0] = spotter
+            prev[1] = 1
+        write_db(caught, score, spot, images)
+        response = client.reactions_add(channel=event['channel'], name="white_check_mark", timestamp=event['ts'])
+
+@bolt_app.message("scoreboard")
+@bolt_app.message("spotboard")
+def scoreboard(event, say):
+    scoreboard = sorted(spot.items(), key=lambda p: p[1], reverse=True)[:5]
+    message = "Spotboard:\n" 
+    for i in range(len(scoreboard)):
+        curr = scoreboard[i]
+        message += f"{i + 1}. <@{curr[0]}> - {curr[1]}\n" 
+    say(message)
+
+@bolt_app.message("caughtboard")
+def caughtboard(event, say):
+    caughtboard = sorted(caught.items(), key=lambda p: p[1], reverse=True)[:5]
+    message = "Caughtboard:\n" 
+    for i in range(len(caughtboard)):
+        curr = caughtboard[i]
+        message += f"{i + 1}. {curr[0]} - {curr[1]}\n" 
+    say(message)
+
+# TODO
+@bolt_app.message("pics")
+def pics(event, say):
+    found_spotted = re.search(USER_PATTERN, event['text'])
+    if not found_spotted:
+        return
+    spotted = found_spotted[0]
+    message = f"Spots of {spotted}:\n"
+    for link in images[spotted]:
+        message += f"{link}\n"
+    print(message)
+
+@bolt_app.event("file_shared")
+@bolt_app.event("message")
+def ignore():
+    pass
+
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=email_db, trigger="interval", seconds=30)
+
+# https://stackoverflow.com/questions/21214270/how-to-schedule-a-function-to-run-every-hour-on-flask
 
 if __name__ == '__main__':
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
     app.run(threaded=True, port=5000)
+    # bolt_app.start(5000)
---
+++ utils.py
@@ -0,0 +1,74 @@
+import json
+import os
+
+caught, score, spot, images = {}, {}, {}, {}
+
+DB_PATH = "db/"
+DB = {
+    "caughtboard.json": caught,
+    "scoreboard.json": score,
+    "spotboard.json": spot,
+    "images.json": images
+}
+
+def init_db():
+    try:
+        os.mkdir(DB_PATH[:-1])
+    except:
+        return
+    for disk in DB:
+        with open(DB_PATH + disk, 'w') as f:
+            json.dump({}, f, indent=4)
+
+def read_db():
+    for disk, memory in DB.items():
+        with open(DB_PATH + disk, 'r') as f:
+            memory.update(json.load(f))
+    return caught, score, spot, images
+
+def write_db(*args):
+    for data, disk in zip(args, DB):
+        with open(DB_PATH + disk, 'w') as f:
+            json.dump(data, f, indent=4)
+
+from email.mime.multipart import MIMEMultipart
+from email.message import EmailMessage
+
+import mimetypes
+import smtplib, ssl
+
+# https://betterprogramming.pub/how-to-send-emails-with-attachments-using-python-dd37c4b6a7fd
+def email_db():
+    # port = 465  # For SSL
+    port = int(os.environ.get("SMTP_PORT")) # For TLS
+    password = os.environ.get("PASSWORD")
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP(os.environ.get("SMTP_ADDRESS"), port) as smtp:
+        smtp.ehlo()
+        smtp.starttls(context=context)
+        smtp.ehlo()
+        
+        sender = os.environ.get("SENDER_ADDRESS")
+        smtp.login(sender, password)
+        
+        message = EmailMessage()
+        message["Subject"] = "cbspotbot data"
+        message["From"] = sender
+        message["To"] = os.environ.get("RECEIVER_ADDRESS")
+
+        for filename in DB:
+            path = f'{DB_PATH}{filename}'
+
+            # Guess the content type based on the file's extension.
+            ctype, encoding = mimetypes.guess_type(path)
+            if ctype is None or encoding is not None:
+                ctype = 'application/octet-stream'
+            maintype, subtype = ctype.split('/', 1)
+
+            with open(path, 'rb') as fp:
+                message.add_attachment(fp.read(), maintype=maintype, subtype=subtype, 
+                                filename=filename)
+        smtp.send_message(message)
